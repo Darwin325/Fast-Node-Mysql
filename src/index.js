f3b676ae0b9121021120006da60fc99e5b7bc268
Lighgen <inventario.lighgen@gmail.com>
2019-11-04 21:03:58 -0500

    Configurando todo el embiente de desarrollo de node js para trabajar con el motor de base de datos mysql

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const path = require('path')
+
+//Inicializaciones
+const app = express()
+
+//Settings
+app.set('port', process.env.PORT || 4000)
+app.set('views', path.join(__dirname, 'views'))
+app.engine('.hbs', exphbs({
+   defaultLayout: 'main',
+   layoutsDir: path.join(app.get('views'), 'layouts'),
+   partialsDir: path.join(app.get('views', 'partials')),
+   extname: '.hbs',
+   helpers: require('./lib/handlebars.js')
+}))
+app.set('view engine', '.hbs')
+
+//Middlewares
+app.use(morgan('dev'))//Ver mensajes por consola
+app.use(express.urlencoded({ extended: false }))//Recibir datos tipo strings desdes las vistas
+app.use(express.json())//Enviar y recibir datos json
+
+//Variables globales
+
+
+//Rutas
+app.use(require('./routes'))
+
+//Archivos publicos
+
+//Iniciando el servidor
+app.listen(app.get('port'), () => {
+   console.log('Server on port', app.get('port'))
+})
